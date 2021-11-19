1b7317c6a5a1a800f04ce0f790540d74732ed757
ltsherin <ltsherin@gc112m37.cs.unb.ca>
2021-11-19 18:29:43 -0400

    V2

--- HelloProject/src/HelloApp.java
+++ HelloProject/src/HelloApp.java
@@ -3,7 +3,7 @@ public class HelloApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-   System.out.println("Hello Lekzin");
+   System.out.println("Hello Lekzin Selden");
 	}
 
 }
